0fd0f599a957858725b773c2292d1e2659cdfa38
shewchuk-eric <temeculastoneworks@msn.com>
2014-01-24 22:02:00 -0800

    Project Start - first version of file containing only the main class.

--- Rectangle of Fortune/src/rectangle/of/fortune/RectangleOfFortune.java
+++ Rectangle of Fortune/src/rectangle/of/fortune/RectangleOfFortune.java
@@ -1,22 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package rectangle.of.fortune;
 
 /**
- *
- * @author Eric
+ * @authors Eric Shewchuk and Kevin Phair
  */
 public class RectangleOfFortune {
-
-    /**
-     * @param args the command line arguments
-     */
+       int players, rounds, letters, found, level;
+        /** players is how many players in game - game proposal suggests 2 or 3
+         * rounds is how many rounds to play - limit to a certain number like 5?
+         * letters is how many letters in the word being played
+         * found is how many letters have been correctly chosen in the word being played
+         */
+       class Player {
+           int playNum;
+           char playName;
+           int score;
+       }
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        
+    }   
+        class Menu {
+            // Display menu choices and direct to other classes
+        }
+        class SetOptions {
+           // Set number of players, rounds, and difficulty
+        }
+        class Play {
+           // Play the game
+        }
 }
