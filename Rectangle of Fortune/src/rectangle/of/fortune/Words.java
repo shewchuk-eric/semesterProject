154b45f5372322ff42cce2dbcbdbcc39a40386e7
loveaphair <pha07002@byui.edu>
2014-01-25 15:46:18 -0800

    Here are some words in arrays under a new Words class

---
+++ Rectangle of Fortune/src/rectangle/of/fortune/Words.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rectangle.of.fortune;
+
+/**
+ *
+ * @author kevinphair
+ */
+public class Words {
+    String[] hardWords = {"ALPHABET","JEALOUS","ENORMITY","HAIKU","JAZZED","JUICEBOX","STUPENDOUS","STIPEND","HAJJ"};
+    String[] easyWords = {"SAMPLE","LOVE","BETTER","INTERNET","SPIDER","WATER","NEST","EAST","SLANTED","SCANNER"};
+    String[] medWords = {"EDIBLE","PEANUT","BRANDING","SPELLING","QUIETLY","AMAZING","AFTER","YUMMY"};
+}
