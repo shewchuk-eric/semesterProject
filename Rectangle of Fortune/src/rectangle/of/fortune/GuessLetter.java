1c5aed8a4828c6fa68647708731146309b9e0729
loveaphair <pha07002@byui.edu>
2014-02-24 23:55:20 -0800

    I did something right!

--- Rectangle of Fortune/src/rectangle/of/fortune/GuessLetter.java
+++ Rectangle of Fortune/src/rectangle/of/fortune/GuessLetter.java
@@ -1,5 +1,6 @@
 
 package rectangle.of.fortune;
+import java.util.Random;
 import java.util.Scanner;
 /**
  *
@@ -7,7 +8,8 @@ import java.util.Scanner;
  */
 public class GuessLetter {
 
-    int goodPick, i, correct, eachPass, playerNumber=0, round;
+    int goodPick, count, spinAmount, i, correct, eachPass, playerNumber=0, round;
+    int[] money = {1000,250,300,350,400,450,500,550,600,650,750,800,900,1250,1500,20,10,5};
     String word,secretWord;
     String validLetters="BACDEFGHIJKLMNOPQRSTUVWXYZ";
     PlayGame scoreTracker = new PlayGame();
@@ -25,10 +27,13 @@ public class GuessLetter {
         playerNumber++;        
         //print out the board
         notDone = false;
+        int rnd = new Random().nextInt(money.length);
+        spinAmount=money[rnd];
         System.out.print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nHere's your word:");
             for (char secretLetter : secretWord.toCharArray()){//iterates over the letters
             if(guesses.indexOf(secretLetter) == -1){
             System.out.print("_ ");
+            count=0;
             notDone = true;
         }
            
@@ -43,8 +48,20 @@ public class GuessLetter {
         }
         
         //get user's guess
-        System.out.print("\nGuesses so far: " + guesses);
-        System.out.print("\nPlayer number "+playerNumber+", enter a letter:");
+        System.out.println("\nGuesses so far: " + guesses);
+        if(spinAmount<10){
+            System.out.println("Bankrupt! You lose all your money!");
+            //scoreTracker multiply score by 0.
+            continue;
+        }
+            else if(spinAmount<100){
+                System.out.println("Sorry! You lose your turn!");
+                continue;
+            }
+            else{
+                System.out.println("Each correct guess gets you $" + spinAmount + " per letter.");
+        }
+        System.out.print("Player number "+playerNumber+", enter a letter:");
         String letter = keyboard.next();
         letter = letter.toUpperCase();
         if(letter.length() != 1){
@@ -63,7 +80,13 @@ public class GuessLetter {
             continue;
         }
         if(guesses.indexOf(letter)<0){
-        guesses += letter;    
+        guesses += letter;
+        char c = letter.charAt(0);
+        for(int i =0; i < secretWord.length(); i++){
+        if(secretWord.charAt(i) == c){
+        count++;
+                }
+            }
         }
         else{
         System.out.println("That letter has already been guessed. Try again.");
@@ -76,6 +99,9 @@ public class GuessLetter {
         }
         else{
            System.out.println("Great Guess!");
+           int pointForGuess = spinAmount * count;
+           System.out.println("$" +spinAmount + " x " + count + " = $" + pointForGuess);
+           //add pointForGuess to scoreTracker
            //add score to player - in PlayGame class
            scoreTracker.keepScore(playerNumber, round);
            playerNumber--;
